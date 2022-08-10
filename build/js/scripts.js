208525d18e59a097477ecfb4879e600aa4ac0d4a
Abbos <abbosvaliev2003@gmail.com>
2022-08-10 23:52:28 +0500

    feat: add animations

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -17,4 +17,58 @@ function accordion() {
         })
     })
 }
-accordion() 
+accordion()
+
+
+// SCROLLREVEAL
+
+ScrollReveal({
+    reset: true,
+    distance: '100px',
+    duration: 2000,
+    delay: 400
+});
+
+ScrollReveal().reveal('.hero__title, .hero__text, .section__content, .form__title', {
+    delay: 1000,
+    interval: 500,
+    origin: 'left'
+});
+ScrollReveal().reveal('.logo', {
+    reset: false,
+    delay: 500,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.section__img, .contact-form', {
+    delay: 500,
+    origin: 'right'
+});
+ScrollReveal().reveal('.navbar', {
+    reset: false,
+    delay: 500,
+    origin: 'right'
+});
+
+ScrollReveal().reveal('.section__title, .accordion__item', {
+    delay: 500,
+    interval: 500,
+    origin: 'top'
+});
+ScrollReveal().reveal('.footer__text, .footer__logo', {
+    delay: 500,
+    interval: 500,
+    origin: 'top'
+});
+
+ScrollReveal().reveal('.hero-btn, .hero__item, .section__text', {
+    delay: 500,
+    interval: 1000,
+    origin: 'bottom'
+});
+
+ScrollReveal().reveal('.section__text', {
+    delay: 200,
+    origin: 'bottom'
+});
+
